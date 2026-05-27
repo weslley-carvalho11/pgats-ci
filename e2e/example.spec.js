bd55abb0b600e9ce4732b075a66e03e3a4a9383d
Weslley <weslleycarvalho344@gmail.com>
2026-05-26 21:18:23 -0300

    removendo o erro dos testes e2e

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -35,7 +35,7 @@ test.describe(`user rides`, async () => {
     await page.getByLabel('Height for person').click();
     await page.getByLabel('Height for person').fill('141');
     await page.getByRole('button', { name: 'Next' }).click();
-    // await expect(page.getByText('1 person (>= 141 cm) for the')).toBeVisible();
-    await expect(page.getByText('2 person (>= 141 cm) for the')).toBeVisible();
+    await expect(page.getByText('1 person (>= 141 cm) for the')).toBeVisible();
+    // await expect(page.getByText('2 person (>= 141 cm) for the')).toBeVisible();
   });
 });
